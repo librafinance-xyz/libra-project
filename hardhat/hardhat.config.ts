a517e3ec9caa0612f74099b7c014ccccc561aaf0
kaz <kzzz123@protonmail.com>
2022-05-31 13:34:21 +0400

    Update hardhat.config.ts

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -497,6 +497,15 @@ const config: HardhatUserConfig = {
         },
       },
       {
+        version: "0.8.11",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
         version: "0.8.13",
         settings: {
           optimizer: {
