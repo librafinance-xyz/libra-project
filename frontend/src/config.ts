3a7a441fe65f2fac8823a9081bdca8801390cd56
LIB <libcreatesweb3@gmail.com>
2022-06-07 19:07:38 +0900

    Open LIBRA by LIBRA-WASTR LP in advance

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -142,7 +142,7 @@ export const bankDefinitions: { [contractName: string]: BankInfo } = {
     createLpLink: 'https://librax.finance/add/'+'0xAeaaf0e2c81Af264101B9129C00F4440cCF0F720'+'/'+'0x467E086e506C76381D3B8C74BfDCE9D3bB36Ca6D',
     site: '',
     sort: 2,
-    closedForStaking: true,
+    closedForStaking: false,
   },
 
   LibraAstarLPLShareRewardPool: {
