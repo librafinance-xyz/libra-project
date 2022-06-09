295e917016e308b3707506481ce5c2ea5506b02e
LIB <libcreatesweb3@gmail.com>
2022-06-10 00:11:14 +0900

    Update dummy tokens name

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -275,7 +275,7 @@ const Home = () => {
                 color="white"
                 variant="contained"
                 style={{ marginRight: '10px' }}
-                onClick={() => registerToken(libra.address, 'LIBRADUMMY', 18)}
+                onClick={() => registerToken(libra.address, 'LIBRADUMMY2', 18)}
               >
                 LIBRA to Wallet
               </Button>
@@ -284,7 +284,7 @@ const Home = () => {
                 color="white"
                 variant="contained"
                 style={{ marginRight: '10px' }}
-                onClick={() => registerToken(LShare.address, 'LSHAREDUMMY', 18)}
+                onClick={() => registerToken(LShare.address, 'LSHAREDUMMY2', 18)}
               >
                 LSHARE to Wallet
               </Button>
--- hardhat/contracts/mocks/LibraDummy.sol
+++ hardhat/contracts/mocks/LibraDummy.sol
@@ -61,7 +61,7 @@ contract LibraDummy is ERC20Burnable, Operator {
     /**
      * @notice Constructs the LIBRA ERC-20 contract.
      */
-    constructor(uint256 _taxRate, address _taxCollectorAddress) public ERC20("LIBRADUMMY", "LIBRADUMMY") {
+    constructor(uint256 _taxRate, address _taxCollectorAddress) public ERC20("LIBRADUMMY2", "LIBRADUMMY2") {
         // Mints 1 LIBRA to contract creator for initial pool setup
         require(_taxRate < 10000, "tax equal or bigger to 100%");
         //require(_taxCollectorAddress != address(0), "tax collector address must be non-zero address");
