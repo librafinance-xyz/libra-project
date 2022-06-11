d8854070c8a6a0a507abd512b11f93c92f783047
LIB <libcreatesweb3@gmail.com>
2022-06-11 16:13:36 +0900

    Update genesis start time on top page

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -180,7 +180,7 @@ const Home = () => {
             <Box p={4}>
               <h2>Welcome to Libra Finance! (TEST VERSION)</h2>
               <Countdown
-                date={1654520400 * 1000} // Monday, June 6, 2022 1:00:00 PM
+                date={1654956000 * 1000} // Saturday, June 11, 2022 2:00:00 PM
                 intervalDelay={0}
                 precision={3}
                 renderer={(props) => (
